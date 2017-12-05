7294f9b368e4e1ef8527f629b536c6f1d78062a5
Joseph Holley <joeholley4@gmail.com>
2017-12-05 12:17:01 +0800

    Change Inc to LLC
    Change-Id: I6a4a7991e57c45f7a61883b4167bbf34b523aa72

--- scaling-manager/tests/test-loader.sh
+++ scaling-manager/tests/test-loader.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2017 Google Inc. All Rights Reserved.
+# Copyright 2017 Google LLC All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,14 +13,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Default Openarena port.
+DEFAULT_PORT=27961
+
 # Check for kubectl in local path.
 command -v kubectl >/dev/null 2>&1 || { echo >&2 "Command kubectl required in $PATH to use this script, exiting"; exit 1; }
 
 # Loop 15 times, start one pod every ~20 seconds.
 for i in $(seq 1 15); do
-  echo "Starting 'openarena.dgs.${i}' DGS pod (replaces any exising pod with the same name)"
+  NEW_PORT=`expr ${DEFAULT_PORT} + ${i}` 
+  echo "Starting 'openarena.dgs.${i}' DGS pod on port ${NEW_PORT} (replaces any exising pod with the same name)"
   kubectl delete pods openarena.dgs.$i 2>&1 | grep -v "NotFound"
   sleep 20 
   sed "s/openarena\.dgs/openarena.dgs.$i/g" "$( cd $(dirname $0) ; \
-    pwd -P )/../../openarena/k8s/openarena-pod.yaml" | kubectl apply -f -
+    pwd -P )/../../openarena/k8s/openarena-pod.yaml" | 
+    sed -e "s/${DEFAULT_PORT}/${NEW_PORT}/g" | kubectl apply -f -
 done
